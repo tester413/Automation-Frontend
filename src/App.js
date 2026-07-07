e80858db5a2704683da345a34973606d1c3f2d5c
Shubhansh <tester@colorpapers.in>
2026-07-07 16:04:01 +0530

    Test frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Profile from "./UserFrontend/Profile";
-//import News from "./UserFrontend/News";
-// import Search from "./UserFrontend/Search";
+import News from "./UserFrontend/News";
+import Search from "./UserFrontend/Search";
 
 function App() {
   return (
     <div>
       <Profile />
       <Search/>
+      <News/>
     </div>
   );
 }
